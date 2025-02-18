a93132aa722d1d7f911955b8fdbf53ba113612be
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-02-18 23:58:33 +0100

    Design the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,16 @@
 function Header() {
-	return <></>;
+	return (
+		<>
+			<img src='' alt='mohamed sayhi personal image' />
+			<h2>Mohamed Sayhi</h2>
+			<p>
+				AI Researcher. Currently working as a Software Developer at
+				Vermeg and finishing my masters thesis - University of Monastir,
+				Tunisia. CS enthusiast at general and Programming lover at
+				heart.
+			</p>
+		</>
+	);
 }
 
 export default Header;
