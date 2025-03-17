e11a4acf6062e3d6e67b5398f119f6cf261f68d5
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-03-17 14:52:08 +0100

    Add routes configuration for navigation

---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import App from './App';
+import Post from './components/Post';
+
+const routes = [
+	{
+		path: '/',
+		element: <App />,
+		children: {
+			path: ':postId',
+			element: <Post />,
+        },
+    },
+    
+    
+];
+
+export default routes;
