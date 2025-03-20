84686c1c2b0fbd3c38a7e4ed63bbadc46b924fdf
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-03-20 12:50:32 +0100

    Add styling to PostPreview component

--- src/components/PostPreview.jsx
+++ src/components/PostPreview.jsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -7,14 +8,43 @@ const Wrapper = styled.div`
 	flex-direction: column;
 	align-items: center;
 	border: 1px solid black;
+	border-radius: 1rem;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	padding: 2rem;
+	margin-bottom: 2rem;
+
+	.date {
+		font-size: 1.3rem;
+		font-weight: 300;
+	}
+
+	p {
+		padding: 2rem;
+		text-align: justify;
+		font-size: 1.7rem;
+	}
+
+	button {
+		padding: 1rem 3rem;
+		font-size: 1.5rem;
+	}
 `;
+
+function previewText(text, wordLimit) {
+	const words = text.split(' ');
+	if (words.length > wordLimit) {
+		return words.slice(0, wordLimit - 1).join(' ') + '...';
+	}
+
+	return text;
+}
 
 function PostPreview({ id, title, content, date }) {
 	return (
 		<Wrapper>
-			<h4>{date}</h4>
+			<div className='date'>{moment(date).format('MMMM Do YYYY')}</div>
 			<h1>{title}</h1>
-			<p>{content}</p>
+			<p>{previewText(content, 50)}</p>
 			<Link to={`posts/${id}`}>
 				<button>Read More</button>
 			</Link>
