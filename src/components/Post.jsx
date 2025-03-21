20d65d6bf1284d56b5207908f1be160de663a525
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-03-21 15:55:05 +0100

    Add styling to Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,6 +2,28 @@ import { useParams } from 'react-router-dom';
 import Header from './Header';
 import { useEffect, useState } from 'react';
 import Bio from './Bio';
+import moment from 'moment';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	.post {
+		margin-top: 1.5rem;
+		.date {
+			font-size: 1.7rem;
+			font-weight: 300;
+			font-style: italic;
+		}
+
+		h1 {
+			font-size: 5.5rem;
+		}
+
+		p {
+			text-align: left;
+			font-size: 2rem;
+		}
+	}
+`;
 
 function Post() {
 	const { postId } = useParams();
@@ -21,13 +43,17 @@ function Post() {
 	if (!post) return 'LOADING';
 
 	return (
-		<>
+		<Wrapper>
 			<Header />
-			<h1>{post.title}</h1>
-			<h4>{post.date}</h4>
-			<p>{post.content}</p>
+			<div className='post'>
+				<h4 className='date'>
+					{moment(post.date).format('MMMM Do YYYY')}
+				</h4>
+				<h1>{post.title}</h1>
+				<p>{post.content}</p>
+			</div>
 			<Bio title={false} />
-		</>
+		</Wrapper>
 	);
 }
 
